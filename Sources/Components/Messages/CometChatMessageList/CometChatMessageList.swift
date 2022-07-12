a70f0f4237c695c99acd0b78659c9dce359d0149
Pushpsen Airekar <pushpsen.airekar@cometchat.com>
2022-07-12 13:18:06 +0530

    added changes for message bubble resizing

--- Sources/Components/Messages/CometChatMessageList/CometChatMessageList.swift
+++ Sources/Components/Messages/CometChatMessageList/CometChatMessageList.swift
@@ -949,8 +949,10 @@ extension CometChatMessageList: UITableViewDelegate, UITableViewDataSource {
               return cell
             }
           } else {
+              
 
-              if let message = message as? TextMessage, let cell = tableView.dequeueReusableCell(withIdentifier: "CometChatTextAutoSizeBubble", for: indexPath) as? CometChatTextAutoSizeBubble && message.deleteAt == 0 {
+              
+              if let message = message as? TextMessage, message.deletedAt == 0.0 , let cell = tableView.dequeueReusableCell(withIdentifier: "CometChatTextAutoSizeBubble", for: indexPath) as? CometChatTextAutoSizeBubble {
               if let configurations = configurations {
                 cell.set(configurations: configurations)
                 cell.customViews = self.customViews
