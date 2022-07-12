4b860f6703795872eca0bf2b45d92f7f00436e20
AbdullahAnsarri <abdullah.ansari@cometchat.com>
2022-07-12 17:48:12 +0530

    reaction background 18

--- Sources/Components/Messages/MessageBubbles/CometChatMessageBubble/CometChatMessageBubble.swift
+++ Sources/Components/Messages/MessageBubbles/CometChatMessageBubble/CometChatMessageBubble.swift
@@ -457,8 +457,8 @@ class CometChatMessageBubble: UITableViewCell {
         background.backgroundColor = .clear
         /// show when message has been deleted.
         if message.deletedAt > 0.0 {
-            containerStackView.addBackground(color: .yellow)
-            background.backgroundColor = .clear
+            containerStackView.addBackground(color: .clear)
+            background.backgroundColor = CometChatTheme.palatte!.background!
             backgroundHeightConstraint.constant = 36
             backgroundWidthConstraint.constant = 173
             let deleteBubble = CometChatDeleteBubble(frame: CGRect(x: 0, y: 0, width: backgroundWidthConstraint.constant, height: backgroundHeightConstraint.constant), message: message, isStandard: isStandard)
