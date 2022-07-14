bb224ee50957ebf8f5d7ee1ce5ac288823f4a75b
AbdullahAnsarri <abdullah.ansari@cometchat.com>
2022-07-14 18:26:27 +0530

    reaction background 146

--- Sources/Components/Shared/CometChatUIHelper.swift
+++ Sources/Components/Shared/CometChatUIHelper.swift
@@ -10,27 +10,27 @@ import CometChatPro
 
 final public class CometChatUIHelper {
     
-    static func onMessageSent(message: BaseMessage, status: MessageStatus) {
+    public static func onMessageSent(message: BaseMessage, status: MessageStatus) {
         CometChatMessageEvents.emitOnMessageSent(message: message, status: status)
     }
     
-    static func onMessageError(message: BaseMessage, error: CometChatException) {
+    public static func onMessageError(message: BaseMessage, error: CometChatException) {
         CometChatMessageEvents.emitOnError(message: message, error: error)
     }
 
-    static func onMessageEdit(message: BaseMessage, status: MessageStatus) {
+    public static func onMessageEdit(message: BaseMessage, status: MessageStatus) {
         CometChatMessageEvents.emitOnMessageEdit(message: message, status: status)
     }
 
-    static func onMessageDelete(message: BaseMessage, status: MessageStatus) {
+    public static func onMessageDelete(message: BaseMessage, status: MessageStatus) {
         CometChatMessageEvents.emitOnMessageDelete(message: message, status: status)
     }
 
-    static func onMessageReply(message: BaseMessage, status: MessageStatus) {
+    public static func onMessageReply(message: BaseMessage, status: MessageStatus) {
         CometChatMessageEvents.emitOnMessageReply(message: message, status: status)
     }
 
-    static func onMessageReact(message: BaseMessage, reaction: CometChatMessageReaction) {
+    public static func onMessageReact(message: BaseMessage, reaction: CometChatMessageReaction) {
         CometChatMessageEvents.emitOnMessageReact(message: message, reaction: reaction)
     }
 }
