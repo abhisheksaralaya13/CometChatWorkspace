8c6395addf11b25c69398470c910cfdfe6aed3c8
AbdullahAnsarri <abdullah.ansari@cometchat.com>
2022-07-14 14:03:31 +0530

    reaction background 115

--- Sources/Components/Messages/MessageBubbles/CometChatTextAutoSizeBubble/CometChatTextAutoSizeBubble.swift
+++ Sources/Components/Messages/MessageBubbles/CometChatTextAutoSizeBubble/CometChatTextAutoSizeBubble.swift
@@ -480,7 +480,7 @@ class CometChatTextAutoSizeBubble: UITableViewCell {
         } else if !reactions.reactions.count.isMultiple(of: numberOfItemInARow) && reactions.reactions.count >= 6 {
             let rows = reactions.reactions.count / numberOfItemInARow + 1
             heightReactions.constant = CGFloat(rows * Int(heightReactions.constant))
-        } else if !reactions.reactions.count.isMultiple(of: numberOfItemInARow) && reactions.reactions.count > 1 && reactions.reactions.count < 6 {
+        } else if !reactions.reactions.count.isMultiple(of: numberOfItemInARow) && reactions.reactions.count > 1 && reactions.reactions.count < 6 { 
             let rows = reactions.reactions.count / numberOfItemInARow + 1
             heightReactions.constant = CGFloat(rows * Int(heightReactions.constant))
         } else if reactions.reactions.count == 0 {
