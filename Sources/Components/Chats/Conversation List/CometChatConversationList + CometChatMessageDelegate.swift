3dc35cf988b9b4516623ad64b2a098e03d9c2e45
Pushpsen Airekar <pushpsen.airekar@cometchat.com>
2022-07-17 14:40:06 +0530

    beta release test 22

--- Sources/Components/Chats/Conversation List/CometChatConversationList + CometChatMessageDelegate.swift
+++ Sources/Components/Chats/Conversation List/CometChatConversationList + CometChatMessageDelegate.swift
@@ -9,7 +9,7 @@ import Foundation
 import CometChatPro
 
 extension CometChatConversationList : CometChatMessageDelegate {
-
+    
     public func onTextMessageReceived(textMessage: TextMessage) {
         if let conversation = CometChat.getConversationFromMessage(textMessage) {
             self.update(conversation: conversation)
@@ -29,12 +29,62 @@ extension CometChatConversationList : CometChatMessageDelegate {
         }
     }
     
-    public func onTypingEnded(_ typingDetails: TypingIndicator) {
-        
+    public func onTypingStarted(_ typingDetails: TypingIndicator) {
+        if let row = self.conversations.firstIndex(where: {($0.conversationWith as? User)?.uid == typingDetails.sender?.uid && $0.conversationType.rawValue == typingDetails.receiverType.rawValue }), let indexPath = IndexPath(row: row, section: 0) as? IndexPath , let conversationListItem = self.tableView.cellForRow(at: indexPath) as? CometChatConversationListItem  {
+            
+            if  (conversationListItem.conversation?.conversationWith as? User)?.uid == typingDetails.sender?.uid {
+                DispatchQueue.main.async {
+                    conversationListItem
+                        .set(typingIndicatorText: "IS_TYPING".localize())
+                        .show(typingIndicator: true)
+                    conversationListItem.reloadInputViews()
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                    conversationListItem.show(typingIndicator: false)
+                    conversationListItem.reloadInputViews()
+                }
+            }else if (conversationListItem.conversation?.conversationWith as? Group)?.guid == typingDetails.receiverID {
+                
+                let user = typingDetails.sender?.name ?? ""
+                DispatchQueue.main.async {
+                    conversationListItem.set(typingIndicatorText: user + " " + "IS_TYPING".localize()).show(typingIndicator: true)
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                    conversationListItem.show(typingIndicator: false)
+                    conversationListItem.reloadInputViews()
+                }
+            }
+        }
     }
     
-    public func onTypingStarted(_ typingDetails: TypingIndicator) {
-        
+    /**
+     This method triggers when real time event for  stop typing received from  CometChat Pro SDK
+     - Parameter typingDetails: This specifies TypingIndicator Object.
+     - Author: CometChat Team
+     - Copyright:  ©  2022 CometChat Inc.
+     - See Also:
+     [CometChatConversationList Documentation](https://prodocs.cometchat.com/docs/ios-ui-screens#section-3-comet-chat-conversation-list)
+     */
+    public func onTypingEnded(_ typingDetails: TypingIndicator) {
+        if let row = self.conversations.firstIndex(where: {($0.conversationWith as? User)?.uid == typingDetails.sender?.uid && $0.conversationType.rawValue == typingDetails.receiverType.rawValue }), let indexPath = IndexPath(row: row, section: 0) as? IndexPath , let conversationListItem = self.tableView.cellForRow(at: indexPath) as? CometChatConversationListItem  {
+            
+            if  (conversationListItem.conversation?.conversationWith as? User)?.uid == typingDetails.sender?.uid {
+                DispatchQueue.main.async {
+                    conversationListItem
+                        .set(typingIndicatorText: "")
+                        .show(typingIndicator: false)
+                    conversationListItem.reloadInputViews()
+                }
+            }else if (conversationListItem.conversation?.conversationWith as? Group)?.guid == typingDetails.receiverID {
+                
+                let user = typingDetails.sender?.name ?? ""
+                DispatchQueue.main.async {
+                    conversationListItem.set(typingIndicatorText: user + " " + "").show(typingIndicator: false)
+                }
+            }
+        }
     }
 }
 
--- Sources/Components/Chats/Conversation List/CometChatConversationList + CometChatUserDelegate.swift
+++ Sources/Components/Chats/Conversation List/CometChatConversationList + CometChatUserDelegate.swift
@@ -11,12 +11,48 @@ import CometChatPro
 
 extension CometChatConversationList : CometChatUserDelegate {
 
+    /**
+     This method triggers users comes online from user list.
+     - Parameter user: This specifies `User` Object
+     - Author: CometChat Team
+     - Copyright:  ©  2022 CometChat Inc.
+     - See Also:
+     [CometChatConversationList Documentation](https://prodocs.cometchat.com/docs/ios-ui-screens#section-3-comet-chat-conversation-list)
+     */
     public func onUserOnline(user: User) {
-        
+        if conversationType == .user || conversationType == .none {
+            if let row = self.conversations.firstIndex(where: {($0.conversationWith as? User)?.uid == user.uid}) {
+                let indexPath = IndexPath(row: row, section: 0)
+                DispatchQueue.main.async {
+                    if let conversationListItem = self.tableView.cellForRow(at: indexPath) as? CometChatConversationListItem {
+                        conversationListItem.set(statusIndicator: .online)
+                        conversationListItem.reloadInputViews()
+                    }
+                }
+            }
+        }
     }
-    
+
+    /**
+     This method triggers users goes offline from user list.
+     - Parameter user: This specifies `User` Object
+     - Author: CometChat Team
+     - Copyright:  ©  2022 CometChat Inc.
+     - See Also:
+     [CometChatConversationList Documentation](https://prodocs.cometchat.com/docs/ios-ui-screens#section-3-comet-chat-conversation-list)
+     */
     public func onUserOffline(user: User) {
-        
+        if conversationType == .user || conversationType == .none {
+            if let row = self.conversations.firstIndex(where: {($0.conversationWith as? User)?.uid == user.uid}) {
+                let indexPath = IndexPath(row: row, section: 0)
+                DispatchQueue.main.async {
+                    if let conversationListItem = self.tableView.cellForRow(at: indexPath) as? CometChatConversationListItem {
+                        conversationListItem.set(statusIndicator: .offline)
+                        conversationListItem.reloadInputViews()
+                    }
+                }
+            }
+        }
     }
 
 }
