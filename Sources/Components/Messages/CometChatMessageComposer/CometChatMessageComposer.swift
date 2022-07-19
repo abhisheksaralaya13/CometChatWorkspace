1876ded25a5a7b6b1e706a8b92f1bc94bcac45b4
Pushpsen Airekar <pushpsen.airekar@cometchat.com>
2022-07-20 00:45:37 +0530

    added changes

--- Sources/Components/Messages/CometChatMessageComposer/CometChatMessageComposer.swift
+++ Sources/Components/Messages/CometChatMessageComposer/CometChatMessageComposer.swift
@@ -726,9 +726,12 @@ enum MessageComposerMode {
             
             let a = Array(Set(filteredMessageTemplates).subtracting(Set(excludeMessageTypes)))
             
-            print(" ddd: \(a)")
+            
+            let reuslt = filteredMessageTemplates.filter { !excludeMessageTypes.contains(obj: $0.type) }
+            print(" reuslt: \(reuslt)")
             
             if !filteredMessageTemplates.isEmpty {
+                
                 for template in filteredMessageTemplates {
                   
                     let actionItem = ActionItem(id: template.type, text: template.name ?? "", icon: template.icon ?? UIImage(), textColor: CometChatTheme.palatte?.accent, textFont: CometChatTheme.typography?.Name2, startIconTint: CometChatTheme.palatte?.accent700)
